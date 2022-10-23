607e54db024bbe8ff5c051e9cf6cd9802176640e
Yura Yura <yuriydmytrukr@gmail.com>
2022-10-23 13:34:34 +0300

    Filter done

--- src/Scripts/Filter.js
+++ src/Scripts/Filter.js
@@ -1,7 +1,12 @@
-function filter(array, search){
-    console.log(search);
-    
-    return array;
+function filter(array, key){   
+    return array.filter(element => checkIfSuitable(element, key))
+}
+
+function checkIfSuitable(element, key){
+    let value = Object.values(element)
+    value.pop()
+    value = value.join("")
+    return value.includes(key);
 }
 
 export default filter
